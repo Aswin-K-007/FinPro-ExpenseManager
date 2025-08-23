87289e3032a78a802b86d1e52d1787177391559a
Roronoa-Zoro-007 <strawhats.rz@gmail.com>
2025-08-23 13:08:05 +0530

    Created Registration Component

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
-export class Register {
+export class RegisterComponent {
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    contactNo: '',
+    password: '',
+    confirmPassword: ''
+  };
 
+  onSubmit(){
+    if (this.user.password !== this.user.confirmPassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+    console.log("User registered:", this.user);
+    alert("Registration Successful!");
+  }
 }
